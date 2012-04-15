6fefeed21e36e7ddddcc9879e550abc9268bbe7a
Armin Pech <mail@arminpech.de>
2012-04-15 19:45:49 +0200

    Added constraints on FKs, moved references to own table + archiving

--- sql/schema-postgres.sql
+++ sql/schema-postgres.sql
@@ -27,75 +27,98 @@ grant connect on database cmdb to cmdb;
 
 -- add schema for storing data
 \connect cmdb
-create schema cmdb authorization cmdb_admin;
-grant all privileges on schema cmdb to cmdb_admin with grant option;
+create schema core authorization cmdb_admin;
+grant all privileges on schema core to cmdb_admin with grant option;
 
 \connect cmdb cmdb_admin
 
 -- value type definition for objects
-create type cmdb.value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob', 'link', 'child');
+create type core.value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob');
 
 -- role type definition for roles
-create type cmdb.role_type_enum as enum ('user', 'group');
+create type core.role_type_enum as enum ('user', 'group');
+
+-- reference type definition for references table
+create type core.ref_type_enum as enum ('parent', 'link');
 
 
 -- objects - Stores all data in objects
-create table cmdb.objects (
+create table core.objects (
 	id		bigserial		not null	primary key,
-	parent_id	bigint			null		references cmdb.objects (id),
 	value		bytea			null		default null,
-	value_type	cmdb.value_type_enum	not null,
+	value_type	core.value_type_enum	not null,
 	name		varchar(80)		not null,
 	version		integer			not null,
 	mtime		timestamp		not null,
-	role_id		integer			null		default null
+	role_id		integer			null		references core.roles (id) default null
 );
 
 -- objects_archive - Archives modified objects as a history
-create table cmdb.objects_archive (
-	id		bigint			not null	references cmdb.objects (id),
-	parent_id	bigint			null		references cmdb.objects (id),
-	value		bytea			null		default null,
-	value_type	cmdb.value_type_enum	not null,
+create table core.objects_archive (
+	id		bigint			not null	references core.objects (id),
+	value		bytea			null,
+	value_type	core.value_type_enum	not null,
 	name		varchar(80)		not null,
 	version		integer			not null,
 	mtime		timestamp		not null,
-	role_id		integer			not null,
+	role_id		integer			not null	references core.roles (id),
+	atime		timestamp		not null
+);
+
+
+-- references - Links the objects togehter
+create table core.references (
+	object_id	bigint			not null	references core.objects (id),
+	ref_object_id	bigint			not null	references core.objects (id),
+	ref_type	core.ref_type_enum	not null,
+	version		integer			not null,
+	mtime		timestamp		not null,
+	role_id		integer			null		references core.roles (id)
+);
+
+-- references_archive - Archives all modified references
+create table core.references_archive (
+	object_id	bigint			not null	references core.objects (id),
+	ref_object_id	bigint			not null	references core.objects (id),
+	ref_type	core.ref_type_enum	not null,
+	version		integer			not null,
+	mtime		timestamp		not null,
+	role_id		integer			not null	references core.roles (id),
 	atime		timestamp		not null
 );
 
 
 -- roles - Defines users and roles for permission handling
-create table cmdb.roles (
+create table core.roles (
 	id		serial			not null	primary key,
-	role_type	cmdb.role_type_enum	not null
+	role_type	core.role_type_enum	not null
 );
 
 -- roles_membership - Assigns roles to roles like groups or user cloning
-create table cmdb.roles_memebership (
-	role_id		integer			not null	references cmdb.roles (id),
-	add_role_id	integer			not null	references cmdb.roles (id)
+create table core.roles_memebership (
+	role_id		integer			not null	references core.roles (id),
+	add_role_id	integer			not null	references core.roles (id)
 );
 
 -- permissions - Grants permissions for objects to roles
-create table cmdb.permissions (
-	object_id	bigint			not null	references cmdb.objects (id),
-	role_id		integer			not null	references cmdb.roles (id),
+create table core.permissions (
+	object_id	bigint			not null	references core.objects (id),
+	role_id		integer			not null	references core.roles (id),
 	permission	smallint		not null	check (permission >= 0) default 0
 );
 
 -- permissions_audit - Save the changed permissions as a trace
-create table cmdb.permissions_audit (
-	object_id	bigint			not null	references cmdb.objects (id),
-	role_id		integer			not null	references cmdb.roles (id),
+create table core.permissions_audit (
+	object_id	bigint			not null	references core.objects (id),
+	role_id		integer			not null	references core.roles (id),
 	permission	smallint		not null	check (permission >= 0),
-	changed_by_role	integer			not null	references cmdb.roles (id),
+	changed_by_role	integer			not null	references core.roles (id),
 	atime		timestamp		not null	default now()
 );
 
 
 -- options - Stores application internal options
-create table cmdb.options (
+create table core.options (
 	name		varchar(160)		not null	primary key,
-	object_id	bigint			not null	references cmdb.objects (id)
+	object_id	bigint			not null	references core.objects (id)
 );
