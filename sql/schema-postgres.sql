b3da85718e90fc0c02255772d61a19dd719a2c05
Armin Pech <mail@arminpech.de>
2012-04-15 15:07:17 +0200

    Added schema to store databsae content

--- sql/schema-postgres.sql
+++ sql/schema-postgres.sql
@@ -20,26 +20,31 @@
 -- create schema and grant to cmdb admin
 create role cmdb_admin login password 'cmdb_admin';
 create database cmdb owner cmdb_admin;
-grant all privileges on database cmdb to cmdb_admin with grant option;
 
 -- create user for cmdb application
 create role cmdb login password 'cmdb';
 grant connect on database cmdb to cmdb;
 
+-- add schema for storing data
+\connect cmdb
+create schema cmdb authorization cmdb_admin;
+grant all privileges on schema cmdb to cmdb_admin with grant option;
+
+\connect cmdb cmdb_admin
 
 -- value type definition for objects
-create type value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob', 'link', 'child');
+create type cmdb.value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob', 'link', 'child');
 
 -- role type definition for roles
-create type role_type_enum as enum ('user', 'group');
+create type cmdb.role_type_enum as enum ('user', 'group');
 
 
 -- objects - Stores all data in objects
-create table objects (
+create table cmdb.objects (
 	id		bigserial		not null	primary key,
-	parent_id	bigint			null		references objects (id),
+	parent_id	bigint			null		references cmdb.objects (id),
 	value		bytea			null		default null,
-	value_type	value_type_enum		not null,
+	value_type	cmdb.value_type_enum	not null,
 	name		varchar(80)		not null,
 	version		integer			not null,
 	mtime		timestamp		not null,
@@ -47,11 +52,11 @@ create table objects (
 );
 
 -- objects_archive - Archives modified objects as a history
-create table objects_archive (
-	id		bigint			not null	references objects (id),
-	parent_id	bigint			null		references objects (id),
+create table cmdb.objects_archive (
+	id		bigint			not null	references cmdb.objects (id),
+	parent_id	bigint			null		references cmdb.objects (id),
 	value		bytea			null		default null,
-	value_type	value_type_enum		not null,
+	value_type	cmdb.value_type_enum	not null,
 	name		varchar(80)		not null,
 	version		integer			not null,
 	mtime		timestamp		not null,
@@ -61,36 +66,36 @@ create table objects_archive (
 
 
 -- roles - Defines users and roles for permission handling
-create table roles (
+create table cmdb.roles (
 	id		serial			not null	primary key,
-	role_type	role_type_enum		not null
+	role_type	cmdb.role_type_enum	not null
 );
 
 -- roles_membership - Assigns roles to roles like groups or user cloning
-create table roles_memebership (
-	role_id		integer			not null	references roles (id),
-	add_role_id	integer			not null	references roles (id)
+create table cmdb.roles_memebership (
+	role_id		integer			not null	references cmdb.roles (id),
+	add_role_id	integer			not null	references cmdb.roles (id)
 );
 
 -- permissions - Grants permissions for objects to roles
-create table permissions (
-	object_id	bigint			not null	references objects (id),
-	role_id		integer			not null	references roles (id),
+create table cmdb.permissions (
+	object_id	bigint			not null	references cmdb.objects (id),
+	role_id		integer			not null	references cmdb.roles (id),
 	permission	smallint		not null	check (permission >= 0) default 0
 );
 
 -- permissions_audit - Save the changed permissions as a trace
-create table permissions_audit (
-	object_id	bigint			not null	references objects (id),
-	role_id		integer			not null	references roles (id),
+create table cmdb.permissions_audit (
+	object_id	bigint			not null	references cmdb.objects (id),
+	role_id		integer			not null	references cmdb.roles (id),
 	permission	smallint		not null	check (permission >= 0),
-	changed_by_role	integer			not null	references roles (id),
+	changed_by_role	integer			not null	references cmdb.roles (id),
 	atime		timestamp		not null	default now()
 );
 
 
 -- options - Stores application internal options
-create table options (
+create table cmdb.options (
 	name		varchar(160)		not null	primary key,
-	object_id	bigint			not null	references objects (id)
+	object_id	bigint			not null	references cmdb.objects (id)
 );
