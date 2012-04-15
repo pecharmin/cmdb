4e17339a533179cc2d964f89acc189736ee21c29
Armin Pech <mail@arminpech.de>
2012-04-15 12:54:54 +0200

    Added license info to pqsql schema

--- sql/schema-postgres.sql
+++ sql/schema-postgres.sql
@@ -1,5 +1,22 @@
 ---- SQL Script creates needed tables for CMDB
 
+--    CMDB stores any data type in a object tree as attributes.
+--    Copyright (C) 2012 Armin Pech <mail@arminpech.de>, Duesseldorf, Germany
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 -- value type definition for objects
 create type value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob');
 
