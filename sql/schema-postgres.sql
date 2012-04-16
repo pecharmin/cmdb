6592e17c044c728c548a64008b35ba3e009aad97
Armin Pech <mail@arminpech.de>
2012-04-16 21:40:10 +0200

    Optimized schema, fixed references for archving, added tag table

--- sql/schema-postgres.sql
+++ sql/schema-postgres.sql
@@ -17,20 +17,26 @@
 --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
--- create schema and grant to cmdb admin
+-- create database for application and grant to cmdb admin
 create role cmdb_admin login password 'cmdb_admin';
+-- cmdb database objects are owned due security reasons by another user
+-- than which the application uses
 create database cmdb owner cmdb_admin;
 
 -- create user for cmdb application
 create role cmdb login password 'cmdb';
 grant connect on database cmdb to cmdb;
 
--- add schema for storing data
+
+-- add schema for storing data of cmdb core
 \connect cmdb
 create schema core authorization cmdb_admin;
 grant all privileges on schema core to cmdb_admin with grant option;
+grant usage on schema core to cmdb;
 
+-- connect as cmdb admin to create objects
 \connect cmdb cmdb_admin
+
 
 -- value type definition for objects
 create type core.value_type_enum as enum ('object', 'integer', 'double', 'text', 'blob');
@@ -39,87 +45,96 @@ create type core.value_type_enum as enum ('object', 'integer', 'double', 'text',
 create type core.role_type_enum as enum ('user', 'group');
 
 -- reference type definition for references table
-create type core.ref_type_enum as enum ('parent', 'link');
+create type core.reference_type_enum as enum ('parent', 'link');
 
 
--- roles - Defines users and roles for permission handling
+-- roles - Defines users and groups as roles for permission handling
 create table core.roles (
-	id		serial			not null	primary key,
-	role_type	core.role_type_enum	not null
+	id			serial			not null	primary key,
+	role_type		core.role_type_enum	not null
 );
 
--- roles_membership - Assigns roles to roles like groups or user cloning
-create table core.roles_memebership (
-	role_id		integer			not null	references core.roles (id),
-	add_role_id	integer			not null	references core.roles (id)
+-- roles_membership - Assigns roles to roles like grouping or user cloning
+create table core.roles_membership (
+	role_id			integer			not null	references core.roles (id),
+	granted_role_id		integer			not null	references core.roles (id)
 );
 
 
 -- objects - Stores all data in objects
 create table core.objects (
-	id		bigserial		not null	primary key,
-	value		bytea			null		default null,
-	value_type	core.value_type_enum	not null,
-	name		varchar(80)		not null,
-	version		integer			not null,
-	mtime		timestamp		not null,
-	role_id		integer			null		references core.roles (id) default null
+	id			bigserial		not null	primary key,
+	value			bytea			null		default null,
+	value_type		core.value_type_enum	not null,
+	name			varchar(120)		not null,
+	version			integer			not null,
+	mtime			timestamp		not null,
+	locked_by_role_id	integer			null		references core.roles (id) default null
 );
 
 -- objects_archive - Archives modified objects as a history
 create table core.objects_archive (
-	id		bigint			not null	references core.objects (id),
-	value		bytea			null,
-	value_type	core.value_type_enum	not null,
-	name		varchar(80)		not null,
-	version		integer			not null,
-	mtime		timestamp		not null,
-	role_id		integer			not null	references core.roles (id),
-	atime		timestamp		not null
+	id			bigint			not null,
+	value			bytea			null,
+	value_type		core.value_type_enum	not null,
+	name			varchar(120)		not null,
+	version			integer			not null,
+	mtime			timestamp		not null,
+	modified_by_role_id	integer			not null	references core.roles (id)
 );
 
 
 -- references - Links the objects togehter
 create table core.references (
-	object_id	bigint			not null	references core.objects (id),
-	ref_object_id	bigint			not null	references core.objects (id),
-	ref_type	core.ref_type_enum	not null,
-	version		integer			not null,
-	mtime		timestamp		not null,
-	role_id		integer			null		references core.roles (id)
+	object_id		bigint			not null	references core.objects (id),
+	referenced_object_id	bigint			null		references core.objects (id),
+	reference_type		core.reference_type_enum not null,
+	version			integer			not null,
+	mtime			timestamp		not null,
+	locked_by_role_id	integer			null		references core.roles (id) default null,
+	primary key(object_id, referenced_object_id, reference_type)
 );
 
 -- references_archive - Archives all modified references
 create table core.references_archive (
-	object_id	bigint			not null	references core.objects (id),
-	ref_object_id	bigint			not null	references core.objects (id),
-	ref_type	core.ref_type_enum	not null,
-	version		integer			not null,
-	mtime		timestamp		not null,
-	role_id		integer			not null	references core.roles (id),
-	atime		timestamp		not null
+	object_id		bigint			not null,
+	referenced_object_id	bigint			null,
+	reference_type		core.reference_type_enum not null,
+	version			integer			not null,
+	mtime			timestamp		not null,
+	modified_by_role_id	integer			not null	references core.roles (id)
 );
 
 
 -- permissions - Grants permissions for objects to roles
 create table core.permissions (
-	object_id	bigint			not null	references core.objects (id),
-	role_id		integer			not null	references core.roles (id),
-	permission	smallint		not null	check (permission >= 0) default 0
+	object_id		bigint			not null	references core.objects (id),
+	role_id			integer			not null	references core.roles (id),
+	permission		smallint		not null	check (permission >= 0) default 0,
+	mtime			timestamp		not null,
+	granted_by_role_id	integer			not null	references core.roles (id),
+	primary key(object_id, role_id)
 );
 
--- permissions_audit - Save the changed permissions as a trace
-create table core.permissions_audit (
-	object_id	bigint			not null	references core.objects (id),
-	role_id		integer			not null	references core.roles (id),
-	permission	smallint		not null	check (permission >= 0),
-	changed_by_role	integer			not null	references core.roles (id),
-	atime		timestamp		not null	default now()
+-- permissions_audit - Save the changed permissions as a audit
+create table core.permissions_archive (
+	object_id		bigint			not null,
+	role_id			integer			not null	references core.roles (id),
+	permission		smallint		not null	check (permission >= 0),
+	mtime			timestamp		not null,
+	granted_by_role_id	integer			not null	references core.roles (id)
 );
 
 
--- options - Stores application internal options
+-- options - Stores application internal options with human readable names
 create table core.options (
-	name		varchar(160)		not null	primary key,
-	object_id	bigint			not null	references core.objects (id)
+	name			varchar(120)		not null	primary key,
+	object_id		bigint			not null	references core.objects (id)
+);
+
+
+-- tags - Adds unique tag names to an object for human identification
+create table core.tags (
+	name			varchar(120)		not null	primary key,
+	object_id		bigint			not null	references core.objects (id)
 );
