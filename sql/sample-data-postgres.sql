e52c175f1e830b410ec974439dc7a4d5d3a5d03a
Armin Pech <mail@arminpech.de>
2012-04-19 20:35:06 +0200

    Added some sample data for testing new stuff

---
+++ sql/sample-data-postgres.sql
@@ -0,0 +1,9 @@
+-- Insert role sample data
+insert into core.roles (role_type) values ('user');
+insert into core.roles (role_type) values ('user');
+insert into core.roles (role_type) values ('group');
+insert into core.roles (role_type) values ('user');
+insert into core.roles (role_type) values ('group');
+
+-- Insert object sample data template
+--insert into core.objects (value, value_type, name, version, mtime) values ('', '', '', 1, now());
