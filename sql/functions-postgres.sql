18b5c3acfd4021be286d325067e632c52a27f9b9
Armin Pech <mail@arminpech.de>
2012-04-19 20:30:48 +0200

    Finished object handling, added references insert func

--- sql/functions-postgres.sql
+++ sql/functions-postgres.sql
@@ -71,7 +71,7 @@ $$ language sql security definer;
 grant execute on function core.object_insert (bytea, core.value_type_enum, varchar(120), integer) to cmdb;
 
 
--- delete object
+-- delete object by id
 -- Usage: core.object_delete(id, role_id)
 -- Returns: version of deleted object
 create or replace function core.object_delete (
@@ -94,16 +94,16 @@ create or replace function core.object_delete (
 		version,
 		mtime,
 		$2
-	from core.objects where id=$1;
+	from core.objects where id=$1 and locked_by_role_id is null or locked_by_role_id=$2;
 
-	delete from core.objects where id=$1
+	delete from core.objects where id=$1 and locked_by_role_id is null or locked_by_role_id=$2
 	returning version as version;
 $$ language sql security definer;
 
 grant execute on function core.object_delete (bigint, integer) to cmdb;
 
 
--- update object
+-- update object by id
 -- Usage: core.object_update(id, value, value_type, name, role_id)
 -- Returns: version of new object
 create or replace function core.object_update (
@@ -129,7 +129,7 @@ create or replace function core.object_update (
 		version,
 		mtime,
 		$5
-	from core.objects where id=$1;
+	from core.objects where id=$1 and locked_by_role_id is null or locked_by_role_id=$5;
 
 	update core.objects set
 		value			= $2,
@@ -137,8 +137,76 @@ create or replace function core.object_update (
 		name			= $4,
 		version			= version + 1,
 		mtime			= now()
-	where id=$1
+	where id=$1 and locked_by_role_id is null or locked_by_role_id=$5
 	returning version as version;
 $$ language sql security definer;
 
 grant execute on function core.object_update (bigint, bytea, core.value_type_enum, varchar(120), integer) to cmdb;
+
+
+-- select object by id
+-- Usage: core.object_select(id)
+-- Returns: row of core.objects table
+create or replace function core.object_select (
+	bigint
+) returns table (
+	id bigint,
+	value bytea,
+	value_type core.value_type_enum,
+	name varchar(120),
+	version integer,
+	mtime timestamp without time zone,
+	locked_by_role_id integer
+) as $$
+	select * from core.objects where id=$1;
+$$ language sql security definer;
+
+grant execute on function core.object_select (bigint) to cmdb;
+
+
+
+---- reference handling
+
+-- insert reference
+-- Usage: core.reference_insert(object_id, refed_object_id, type, role_id)
+-- Returns: object_id
+create or replace function core.reference_insert (
+	bigint,
+	bigint,
+	core.reference_type_enum,
+	integer
+) returns bigint as $$
+	insert into core.references (
+		object_id,
+		referenced_object_id,
+		reference_type,
+		version,
+		mtime,
+		locked_by_role_id
+	) values (
+		$1,
+		$2,
+		$3,
+		1,
+		now(),
+		null
+	);
+
+	insert into core.references_archive (
+		object_id,
+		referenced_object_id,
+		reference_type,
+		version,
+		mtime,
+		modified_by_role_id
+	) values (
+		$1,
+		$2,
+		$3,
+		0,
+		now(),
+		$4
+	) returning object_id as object_id;
+$$ language sql security definer;
+
+grant execute on function core.reference_insert (bigint, bigint, core.reference_type_enum, integer) to cmdb;
